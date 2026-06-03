bbaf8f199ead7d2f17200910e745bf201de15a5b
kunjamd68-cloud <kunjamd68@gmail.com>
2026-06-03 11:16:51 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "avm-school-v2";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/images/logo.png"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+
+self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+);
+})
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+caches.match(event.request)
+.then(response => {
+return response || fetch(event.request);
+})
+);
+
+});
